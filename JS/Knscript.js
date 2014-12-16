452cecf0cbf2282a73f891d35fc33dcafd896789
HoffsMH <hecker.matthew@gmail.com>
2014-12-16 03:56:57 -0600

    cleaned up initial JS bootstrap classes, started the category object

--- JS/Knscript.js
+++ JS/Knscript.js
@@ -1,40 +1,59 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    $( "#Knpagebtn" ).click(function() {
+
+      Knclearfront();
+    });
+
+    var blacksmithing = new Kncategory("blacksmithing")
+    console.log(blacksmithing)
+    
 
 });
+
+// ====================================================
+// This is the category class  each category takes up
+// a bootstrap pannel
+// ====================================================
+function Kncategory(name) {
+  this.name = name;
+
+
+
+
+};
+
+
+var Knclearfront = function () {
 // ======================================
 // all this is clearing the page after the user presses the 
 // page button to bring up the knowledge map
-// just rearranging and hiding alot of elements
+// just rearranging and hiding elements
 // ======================================
-$( "#Knpagebtn" ).click(function() {
+
   $( "#Knheader" ).animate({
     height: 0
   }, 200, function() {
     // Animation complete.
+    $("#Knheader").hide();
   });
-});
 
 
 
-$( "#Knpagebtn" ).click(function() {
-  $( "#Knshowme" ).animate({
+  $( "#Knaboutrow" ).animate({
     opacity: 0,
   }, 200, function() {
     // Animation complete.
-    $("#Knshowme").hide()
+    $("#Knaboutrow").hide();
   });
-});
 
-$( "#Knpagebtn" ).click(function() {
-  $( "#Knabout" ).animate({
-    opacity: 0,
-  }, 200, function() {
-    // Animation complete.
-    $("#Knabout").hide()
-  });
-});
+  // all of the knowledge page is contained in this element
+  $("#Knpgcontent").show();
+
+};
+
+
 
 
 
